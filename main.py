6f20020f39904dd5962b41a013588545db239d8f
Hamza Ansari <ansari10hamza@gmail.com>
2026-02-27 05:00:35 +0530

    test

--- main.py
+++ main.py
@@ -1,39 +1,8 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from retr_and_gen import ask_question   # use your function
 
-app = FastAPI()
+app=FastAPI()
 
-# Request format
-class QueryRequest(BaseModel):
-    question: str
-
-
-# Test route
 @app.get("/")
+
 def home():
-    return {"message": "RAG API is running 🚀"}
-
-
-# Main RAG route
-@app.post("/ask")
-def ask_question_api(request: QueryRequest):
-    try:
-        response = ask_question(request.question)   # ✅ use custom logic
-
-        return {
-            "question": request.question,
-            "answer": response["answer"],
-            "sources": response["sources"]   # ✅ include sources
-        }
-
-    except Exception as e:
-        return {"error": str(e)}
-
-
-import uvicorn
-import os
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    uvicorn.run(app, host="0.0.0.0", port=port)    
+    return{"messages":"working"}
