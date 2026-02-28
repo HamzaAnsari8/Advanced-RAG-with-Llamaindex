04af1757afb87e510871be166a8cb6a94b8d3c2c
Hamza Ansari <ansari10hamza@gmail.com>
2026-03-01 03:43:13 +0530

    debugging

--- main.py
+++ main.py
@@ -15,8 +15,16 @@ def home():
     print("Home route working")
     return {"message": "API is running"}
 
-# Lazy import (IMPORTANT FIX)
 @app.post("/ask")
 def ask(query: QueryRequest):
-    print("request received:",query.question)
-    return {"answer": "test working"}
+    print("Request received:", query.question)
+
+    try:
+        from retr_and_gen import ask_question
+        response = ask_question(query.question)
+        print("RAG response:", response)
+        return response
+
+    except Exception as e:
+        print("ERROR:", str(e))
+        return {"error": str(e)}
